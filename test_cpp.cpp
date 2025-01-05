49f3174127d31cb9c7ca37c42217622758c1956e
Rob <r_mcinnis@solidice.com>
2025-01-05 00:49:06 -0500

    added timed random string creation

--- test_cpp.cpp
+++ test_cpp.cpp
@@ -328,6 +328,8 @@ void test_thing()
 #endif
 
 //------------------------------------------------------------------------------------------
+#ifdef DONT_USE_TEST_FILE_PARSE
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -681,6 +683,56 @@ void test_file_parse(const char* fname)
   }
 } // :: test_file_parse
 
+#endif // DONT_USE_TEST_FILE_PARSE
+
+//------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <chrono>
+
+void process_strings()
+{
+  printf("process string test (c++ version)\n");
+  srand(time(NULL)) ;
+
+  char rand_buf[1024+1] ;
+  char strings[1000][256] ;
+  char *ch = rand_buf ;
+  auto p1 = std::chrono::high_resolution_clock::now() ;
+
+  // random character block
+  for (int i = 0; i < 1024; i++) {
+    *ch++ = 'A' + rand() % (int)('z' - 'A') ;
+  }
+  rand_buf[1024] = '\0' ;
+  auto p2 = std::chrono::high_resolution_clock::now() ;
+
+  // generate strings
+  for (int i = 0; i < 1000; i++) {
+    int16_t  n = (rand() % 100) + 50 ; // random length
+    int16_t  p = (rand() % 512) + 256 ; // random starting point in the block
+    memcpy( strings[i], &rand_buf[p], n ) ;
+    strings[i][n] = '\0'; 
+  }
+  auto p3 = std::chrono::high_resolution_clock::now() ;
+
+  // now find average length
+  int32_t cnt = 0 ;
+  for (int i = 0; i < 1000; i++) {
+    cnt += strlen( strings[i] ) ;
+  }
+  auto p4 = std::chrono::high_resolution_clock::now() ;
+
+  printf( "time to produce random block: %ld ns \n", (p2-p1).count() ) ;
+  printf( "time to produce random strings: %ld ns \n", (p3-p2).count() ) ;
+  printf( "time to calculate average string length: %ld ns \n", (p4-p3).count() ) ;
+  printf( "average length is: (%d)  %6.2f characters\n", cnt, (float)cnt / 1000.0 ) ;
+
+} // :: process_strings
+
 //------------------------------------------------------------------------------------------
 int main()
 {
@@ -695,7 +747,9 @@ int main()
 //  test_splitstr ();
 //  test_splitstr2();
 
-  test_file_parse( "pack0_core_xga.big" );
+//  test_file_parse( "pack0_core_xga.big" );
+
+  process_strings() ;
 
   return 0;
 } // :: main
